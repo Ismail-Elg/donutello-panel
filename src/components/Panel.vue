53aa4b40d7faa01401f5acd8cae27d7768afc2b1
Ismail Elg <elghamri.ismail@gmail.com>
2022-12-13 20:44:11 +0100

    feat: fetch donut

---
+++ src/components/Panel.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <h1>My Donuts</h1>
+      <ul>
+        <li v-for="donut in donuts" key="donut.id">
+            {{ donut.donut.user.name }}
+            {{ donut.donut.user.email }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        donuts: []
+      }
+    },
+    created() {
+      fetch('http://localhost:3000/api/v1/donuts')
+        .then(response => response.json())
+        .then(data => {
+            this.donuts = data;
+            console.log(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+  </script>
+  
